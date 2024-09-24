ece370948d0078c05191318897946cacb5c27683
liimjiin <limiary27@gmail.com>
2024-09-24 12:16:35 +0900

    Linux server setup

--- src/main/java/security/WebConfig.java
+++ src/main/java/security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-            .allowedOrigins("http://localhost:5174") // 허용할 도메인
+            .allowedOrigins("http://localhost:5173") // 허용할 도메인
             .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
             .allowedHeaders("*") // 허용할 헤더
             .allowCredentials(true) // 인증 정보를 허용할지 여부
